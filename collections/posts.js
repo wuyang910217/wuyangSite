bc1b230a5f2c381bae278368f1239ad0edaf1fab
wuyang910217 <wuyang910217@gmail.com>
2016-02-20 14:53:21 +0800

    添加alethes:page包 增加文章列表的分页pagination

--- collections/posts.js
+++ collections/posts.js
@@ -1,2 +1,13 @@
 Posts = new Mongo.Collection('posts');
 
+Pages = new Meteor.Pagination(Posts, {
+	router: 'iron-router',
+	homeRoute: '/posts/',
+	routerTemplate: 'allPost',
+	itemTemplate: 'itemPost',
+	route: '/posts/page',
+	templateName: 'allPost',
+	perPage: 10,
+	routerLayout: 'mainLayout',
+	sort: {publishedOn: -1},
+});
